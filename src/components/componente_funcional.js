d004f503539bf875504c06c9b4189b1999cbd093
garzaro <clebergarzaro74@gmail.com>
2025-02-04 18:28:50 -0300

    componente funcional

---
+++ src/components/componente_funcional.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+/*componentre funcional*/
+function MeuComponenteFuncional(props) {
+
+    return (
+        <div>
+            
+            <h1>Olá DeepSeek!!!</h1>
+            
+            <p>{props.inseta}, este é um componente funcional.</p>
+            
+        </div>
+        
+    );
+}
+
+export default MeuComponenteFuncional;
+
+ 
