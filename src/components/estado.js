758d990cf3421e4cf4cad5ec93e1181656c9aa9b
garzaro <clebergarzaro74@gmail.com>
2025-02-04 18:25:10 -0300

    componente de classe com estado

---
+++ src/components/estado.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Estado extends Component{
+    /*metodo construtor*/
+    constructor(props){
+        /**pegar todas as props */
+        super(props);
+        /**pegar os estados*/
+        this.state = {
+
+        }
+    }
+    
+    render(){
+        return(
+
+           <div></div> 
+
+        );
+    }
+}
