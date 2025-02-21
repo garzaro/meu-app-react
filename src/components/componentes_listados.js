e2188241838068d8bdc8976ae5ff4d542255a671
garzaro <clebergarzaro74@gmail.com>
2025-02-20 22:55:49 -0300

    adicionando componetes_listados.js

---
+++ src/components/componentes_listados.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Login from "../views/login";
+import CadastroUsuario from "../views/cadastroUsuario";
+
+
+
+function ComponentesListados() {
+
+    return (
+
+        <div>
+
+            <CadastroUsuario/>
+
+
+        </div>
+
+    );
+}
+export default ComponentesListados;
