6f9a680a74b07cdb433d36a9b1fbeb5ab37e86bc
garzaro <clebergarzaro74@gmail.com>
2025-02-20 22:56:56 -0300

    ajustes

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import ComponentesListados from "./components/componentes_listados";
+
+import 'bootswatch/dist/flatly/bootstrap.css'
+import './custom.css'
+
+function App() {
+
+  return (
+
+      <>
+          <ComponentesListados/>
+      </>
+
+  );
+}
+
+export default App;
+/*App.js renderiza os componentes*/
